fe033297e6d0138ef61b36bb34e6a0943affd300
vasilvas99 <ivanovvasil99@gmail.com>
2022-10-13 12:44:45 +0300

    Some reformating

--- simulator.py
+++ simulator.py
@@ -4,11 +4,28 @@ from time import monotonic
 
 class SimulatedCar:
     """
-    A simple (black-box) car kinematics simulator. It takes as input: gas position (0, 1), brake position (0,1) and steering angle (-pi/2, pi/2).
-    It is assumed that breaking and accelerating are two antagonistic effects that can't happend at the same time and that they are related linearly
-    to the acceleration (this models the "jerk" when driving a car).
-    This assumption is then used in combination with the bicycle vehicle model (see: https://thef1clan.com/2020/09/21/vehicle-dynamics-the-kinematic-bicycle-model/)
-    to propage the simulated car in 2D space. After each propagation step velocity and acceleration componets are calculated.
+    A simple (black-box) car kinematics simulator. It takes as input: 
+    accelerator position (0, 1), brake position (0,1) 
+    and steering angle (-pi/2, pi/2).
+    It is assumed that breaking and accelerating are two antagonistic 
+    effects that can't happend at the same time and that they 
+    are related linearly to the acceleration 
+    (this models the "jerk" when driving a car).
+    This assumption is then used in combination with the bicycle vehicle model 
+    (see: https://thef1clan.com/2020/09/21/vehicle-dynamics-the-kinematic-bicycle-model/)
+    to propage the simulated car in 2D space. 
+    After each propagation step velocity and acceleration componets are calculated.
+
+    Example:
+    ----------
+    >>> from simulator import SimulatedCar
+    >>> c = SimulatedCar()
+    >>> c.steer_angle = math.pi/4
+    >>> c.accelerator_position = 0.2
+    >>> while True:
+    >>>     c.update_car()
+    >>>     print(f"{car.position=}")
+
     """
 
     def __init__(
@@ -47,11 +64,14 @@ class SimulatedCar:
         self._accel_x = 0.0  # m/s^2
         self._accel_y = 0.0  # m/s^2
 
-        # these two are needed in order to calculate the components of the acceleration at the end of the update cycle
+        # these two are needed in order to calculate the components of
+        # the acceleration at the end of the update cycle
         self._old_v_x = self._v_x
         self._old_v_y = self._v_y
 
-    # Setup proper r/w access to the interal state, returning vector quantities as (x,y) tuples and scalars as a single value
+    # Setup proper r/w access to the interal state,
+    # returning vector quantities as (x,y) tuples
+    # and scalars as a single value
     @property
     def steer_angle(self):
         return self._steer_angle
@@ -116,10 +136,16 @@ class SimulatedCar:
         return math.sqrt(self._accel_x**2 + self._accel_y**2)
 
     def acc_from_ctrl(self):
-        """Gas and brake are combined in a single variable "control position" that can take any value in the interval [-1,1].
-        An implicit assumption here is that you cannot press the gas and the brake at the same time.
-        Where ctrl_position  > 0 implies acceleration and ctrl_position < 0 implies brakeing. A pieciewise linear relation betwen
-        the ctrl_position and the "signed norm" of the acceleration (> 0 - acceleration, < 0 - brakeing) is constructed based on the values
+        """Gas and brake are combined in a single variable 
+        "control position" that can take any value in the interval [-1,1].
+        An implicit assumption here is that you cannot press the gas 
+        and the brake at the same time.
+        Where ctrl_position  > 0 implies acceleration and
+        ctrl_position < 0 implies braking. 
+        A pieciewise linear relation betwen the ctrl_position and the 
+        "signed norm" of the acceleration 
+        (> 0 - acceleration, < 0 - braking) 
+        is constructed based on the values
         for max_acceleration and max_decleration provided at initialization.
 
         Args:
@@ -134,7 +160,8 @@ class SimulatedCar:
             return self._max_decleration * max(self._control_pos, -1)
 
     def _cog_turning_angle(self):
-        # The turning angle of the center of gravity. See the angle "beta" in the reference text.
+        # The turning angle of the center of gravity.
+        # See the angle "beta" in the reference text.
         return math.atan(self._lr * math.tan(self._steer_angle) / self._carl_len)
 
     def _rotational_speed(self):
@@ -145,7 +172,8 @@ class SimulatedCar:
         )
 
     def _update_speed(self):
-        # update speed assuming locally linear behaviour of acceleration and drag
+        # update speed assuming locally
+        # linear behaviour of acceleration and drag
         new_speed = (
             self._speed
             + (self.acc_from_ctrl() - self._drag_coef * self._speed)
@@ -178,7 +206,9 @@ class SimulatedCar:
         self._accel_y = (self._v_y - self._old_v_y) / (self._simulation_step)
 
     def update_car(self):
-        # the order of the updates here is important, it should be simul time -> speed -> heading -> velocity -> position -> acceleration -> sys timestamp
+        # the order of the updates here is important,
+        #  it should be:
+        #  simul time -> speed -> heading -> velocity -> position -> acceleration -> sys timestamp
         self._simul_time = self._simul_time + self._simulation_step
         self._update_speed()
         self._update_heading_angle()
@@ -196,6 +226,7 @@ def test_data():
     for i in range(100_000):
         car.update_car()
         print(f"{car.position[0]}\t{car.position[1]}")
+
 
 if __name__ == "__main__":
     test_data()
